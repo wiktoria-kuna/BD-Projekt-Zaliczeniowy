423d160c34faa42c5e28b5d86da3bd56e3aa5679
wiktoria-kuna <Wiktoria.kuna11@gmail.com>
2025-01-01 08:18:11 +0100

    Add files via upload

---
+++ Projekt/Projekt Zaliczeniowy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projekt_Zaliczeniowy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
